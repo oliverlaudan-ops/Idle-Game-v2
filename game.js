3a5f4fc609a18aef62e384ff4785c214fbda75e2
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 12:59:56 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
 import { CurrencyManager } from './src/currencies.js';
-import { LayerManager } from './src/layers.js';
 import { updateUI } from './src/ui.js';
+import { startGameLoop } from './src/gameLoop.js';
 
 export const game = {
-  currencies: new CurrencyManager(),
-  layers: new LayerManager()
+  currencies: new CurrencyManager()
 };
 
-let lastTime = 0;
-
-function gameLoop(time) {
-  const delta = (time - lastTime) / 1000;
-  game.currencies.update(delta);
-  game.layers.checkUnlocks();
-  updateUI(game.currencies, game.layers);
-  lastTime = time;
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('game-container');
+  container.innerHTML = `
+    <div id="currency-display">Punkte: 0</div>
+    <button id="click-btn">Klicken (+1)</button>
+    <div id="generators-container"></div>
+  `;
+  
+  document.getElementById('click-btn').onclick = () => game.currencies.click();
+  
+  const gameLoop = startGameLoop(game, updateUI);
   requestAnimationFrame(gameLoop);
-}
-requestAnimationFrame(gameLoop);
+});
