e4dc086d34d8fb71d5283d8d3887ab14763599c1
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 12:59:07 +0100

    Update gameLoop.js

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,1 +1,13 @@
+export function startGameLoop(game, updateUI) {
+  let lastTime = 0;
 
+  function loop(time) {
+    const delta = Math.min((time - lastTime) / 1000, 0.1);
+    game.currencies.update(delta);
+    updateUI(game);
+    lastTime = time;
+    requestAnimationFrame(loop);
+  }
+
+  return loop;
+}
