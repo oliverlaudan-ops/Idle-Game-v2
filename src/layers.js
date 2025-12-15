dfd12f758dcd86170f95796134b79b54e704b6ac
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 13:04:36 +0100

    Update layers.js

--- src/layers.js
+++ src/layers.js
@@ -1,1 +1,35 @@
+export class LayerManager {
+  constructor() {
+    this.layers = [
+      {
+        id: 1,
+        name: "Basis Layer",
+        unlocked: true,
+        unlockRequirement: 0,
+        generators: []
+      },
+      {
+        id: 2,
+        name: "Prestige Layer", 
+        unlocked: false,
+        unlockRequirement: 1000,
+        generators: []
+      }
+    ];
+    this.currentLayer = 1;
+  }
+
+  checkUnlocks() {
+    const points = game.currencies.points;
+    this.layers.forEach(layer => {
+      if (!layer.unlocked && points >= layer.unlockRequirement) {
+        layer.unlocked = true;
+      }
+    });
+  }
+
+  get unlockedLayers() {
+    return this.layers.filter(layer => layer.unlocked);
+  }
+}
 
