744b9475423a0ffc1f112c8617b5e9c6635c1f69
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 12:43:56 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2,15 +2,18 @@ import { CurrencyManager } from './src/currencies.js';
 import { LayerManager } from './src/layers.js';
 import { updateUI } from './src/ui.js';
 
-const currencies = new CurrencyManager();
-const layers = new LayerManager(currencies);
+export const game = {
+  currencies: new CurrencyManager(),
+  layers: new LayerManager()
+};
+
 let lastTime = 0;
 
 function gameLoop(time) {
   const delta = (time - lastTime) / 1000;
-  currencies.update(delta);
-  layers.checkUnlocks();
-  updateUI(currencies, layers);
+  game.currencies.update(delta);
+  game.layers.checkUnlocks();
+  updateUI(game.currencies, game.layers);
   lastTime = time;
   requestAnimationFrame(gameLoop);
 }
