56b3741f5f2a6877ceda6e111d7617c6de9234b4
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 12:56:39 +0100

    Update currencies.js

--- src/currencies.js
+++ src/currencies.js
@@ -1,8 +1,6 @@
 export class CurrencyManager {
   constructor() {
-    this.currencies = {
-      points: { value: 0, generators: 0, baseProduction: 1 }
-    };
+    this.currencies = { points: { value: 0, generators: 0, baseProduction: 1 } };
   }
 
   update(deltaTime) {
@@ -10,17 +8,13 @@ export class CurrencyManager {
     points.value += points.generators * points.baseProduction * deltaTime;
   }
 
-  // Klick gibt temporäre Punkte
-  click() {
-    this.currencies.points.value += 1;
-  }
+  click() { this.currencies.points.value += 1; }
 
-  // Generator kaufen (nicht mehr addGenerator für Klick)
-  buyGenerator(amount = 1) {
+  buyGenerator() {
     const cost = this.generatorCost();
     if (this.currencies.points.value >= cost) {
       this.currencies.points.value -= cost;
-      this.currencies.points.generators += amount;
+      this.currencies.points.generators += 1;
     }
   }
 
@@ -28,15 +22,9 @@ export class CurrencyManager {
     return Math.floor((this.currencies.points.generators + 1) * 10);
   }
 
-  get points() {
-    return this.currencies.points.value.toFixed(0);
-  }
-
-  get generators() {
-    return this.currencies.points.generators.toFixed(1);
-  }
-
-  get productionPerSecond() {
-    return (this.currencies.points.generators * this.currencies.points.baseProduction).toFixed(1);
+  get points() { return Math.floor(this.currencies.points.value); }
+  get generators() { return this.currencies.points.generators.toFixed(1); }
+  get productionPerSecond() { 
+    return (this.currencies.points.generators * this.currencies.points.baseProduction).toFixed(1); 
   }
 }
