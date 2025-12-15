ae136adf0e00d3b3aa8cff78e9ff5dd62538da10
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-15 12:41:59 +0100

    Update currencies.js

--- src/currencies.js
+++ src/currencies.js
@@ -1,1 +1,21 @@
+export class CurrencyManager {
+  constructor() {
+    this.currencies = {
+      points: { value: 0, generators: 0, baseProduction: 1 }
+    };
+  }
+
+  update(deltaTime) {
+    const points = this.currencies.points;
+    points.value += points.generators * points.baseProduction * deltaTime;
+  }
+
+  addGenerator(amount = 1) {
+    this.currencies.points.generators += amount;
+  }
+
+  get points() {
+    return this.currencies.points.value.toFixed(0);
+  }
+}
 
